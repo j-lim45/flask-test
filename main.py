eb70c4b5c8e3d999b5d6a81d3ed928db51e9fe75
j-lim45 <crimson4511@gmail.com>
2026-02-04 17:14:58 +0800

    added some route

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from flask import Flask, request, jsonify
+import geopandas as gpd
+import osmnx as ox
 
 app = Flask(__name__)
 
+nodes = gpd.read_file("files/angeles_all_nodes.geojson")
+edges = gpd.read_file("files/angeles_transit_edges.geojson")
+
+# Function that defines Specific link to return some json file
 @app.route("/get-user/<userid>")
 def get_user(userid):
     userData = {
@@ -10,11 +16,37 @@ def get_user(userid):
         "email": "johndown@email.com"
     }
 
+    # Variable that gets some extra information like after ?someVar="someValue"
     extra = request.args.get("extra")
     if extra:
         userData["extra"] = extra
 
     return jsonify(userData), 200
+
+
+@app.route("/get-shortest-path/<source>-<destination>")
+def getShortestPath(source, destination):
+    nodes = nodes.set_index("osmid")
+    nodes.index = nodes.index.astype(int)
+    nodes.index.name = "osmid"
+
+    # 2. Ensure edges reference the same IDs
+    edges["u"] = edges["u"].astype(int)
+    edges["v"] = edges["v"].astype(int)
+    edges["key"] = edges["key"].astype(int)
+
+    edges = edges.set_index(["u", "v", "key"])
+
+    # 3. CRS must match
+    nodes = nodes.to_crs(edges.crs)
+
+    G = ox.graph_from_gdfs(nodes, edges)
+
+
+    computedShortestPath = ox.routing.shortest_path(G, source, destination, weight="weight")
+    gdfShortestPath = ox.routing.route_to_gdf(G, computedShortestPath)
+
+    return gdfShortestPath
 
 if __name__ == "__main__":
     app.run(debug=True)
