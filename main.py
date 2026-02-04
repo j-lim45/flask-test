093a29a518e86cb2d3ae451963ced5576d09715a
j-lim45 <crimson4511@gmail.com>
2026-02-04 17:36:47 +0800

    test2

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 def get_user(userid):
     userData = {
         "userId": userid,
-        "name": "John Down.",
+        "name": "John Down 2",
         "email": "johndown@email.com"
     }
 
@@ -29,35 +29,34 @@ def getShortestPath():
     source = request.args.get("source")
     destination = request.args.get("destination")
 
+
+
+    nodes = gpd.read_file("files/angeles_all_nodes.geojson")
+    edges = gpd.read_file("files/angeles_transit_edges.geojson")
+
+    nodes = nodes.set_index("osmid")
+    nodes.index = nodes.index.astype(int)
+    nodes.index.name = "osmid"
+
+    # 2. Ensure edges reference the same IDs
+    edges["u"] = edges["u"].astype(int)
+    edges["v"] = edges["v"].astype(int)
+    edges["key"] = edges["key"].astype(int)
+
+    edges = edges.set_index(["u", "v", "key"])
+
+    # 3. CRS must match
+    nodes = nodes.to_crs(edges.crs)
+
+    G = ox.graph_from_gdfs(nodes, edges)
+
+
+    computedShortestPath = ox.routing.shortest_path(G, source, destination, weight="weight")
+    gdfShortestPath = ox.routing.route_to_gdf(G, computedShortestPath)
+
     return jsonify({
-        "source": source,
-        "destination": destination
+        "The list": gdfShortestPath
     }), 200
-
-    # nodes = gpd.read_file("files/angeles_all_nodes.geojson")
-    # edges = gpd.read_file("files/angeles_transit_edges.geojson")
-
-    # nodes = nodes.set_index("osmid")
-    # nodes.index = nodes.index.astype(int)
-    # nodes.index.name = "osmid"
-
-    # # 2. Ensure edges reference the same IDs
-    # edges["u"] = edges["u"].astype(int)
-    # edges["v"] = edges["v"].astype(int)
-    # edges["key"] = edges["key"].astype(int)
-
-    # edges = edges.set_index(["u", "v", "key"])
-
-    # # 3. CRS must match
-    # nodes = nodes.to_crs(edges.crs)
-
-    # G = ox.graph_from_gdfs(nodes, edges)
-
-
-    # computedShortestPath = ox.routing.shortest_path(G, source, destination, weight="weight")
-    # gdfShortestPath = ox.routing.route_to_gdf(G, computedShortestPath)
-
-    # return gdfShortestPath
 
 if __name__ == "__main__":
     app.run(debug=True)
