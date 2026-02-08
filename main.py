646c52a1ba81921be12848a92e536feb95025781
j-lim45 <crimson4511@gmail.com>
2026-02-08 16:47:26 +0800

    added test method

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ import osmnx as ox
 
 app = Flask(__name__)
 
-
+# Test
+@app.route("/test/<num>")
+def get_user(num):
+    userData = {
+        "Hello World!": num
+    }
+    return jsonify(userData), 200
 
 # Function that defines Specific link to return some json file
 @app.route("/get-user/<userid>")
@@ -26,37 +32,51 @@ def get_user(userid):
 @app.route("/get-shortest-path")
 def getShortestPath():
 
-    source = request.args.get("source")
-    destination = request.args.get("destination")
+    source = int(request.args.get("source"))
+    destination = int(request.args.get("destination"))
 
 
-
+    print("Testing")
     nodes = gpd.read_file("files/angeles_all_nodes.geojson")
     edges = gpd.read_file("files/angeles_transit_edges.geojson")
+    print("Tested")
 
     nodes = nodes.set_index("osmid")
     nodes.index = nodes.index.astype(int)
     nodes.index.name = "osmid"
 
+    print(1)
     # 2. Ensure edges reference the same IDs
     edges["u"] = edges["u"].astype(int)
     edges["v"] = edges["v"].astype(int)
     edges["key"] = edges["key"].astype(int)
-
+    print(2)
     edges = edges.set_index(["u", "v", "key"])
-
+    print(3)
     # 3. CRS must match
     nodes = nodes.to_crs(edges.crs)
-
+    print(4)
     G = ox.graph_from_gdfs(nodes, edges)
+
+    # print(nodes.head())
+    # print(edges.head())
+    print(5)
+
+
 
 
     computedShortestPath = ox.routing.shortest_path(G, source, destination, weight="weight")
+    print(6)
     gdfShortestPath = ox.routing.route_to_gdf(G, computedShortestPath)
+    print(7)
 
-    return jsonify({
-        "The list": gdfShortestPath
-    }), 200
+    import numpy as np
+    for col in gdfShortestPath.columns:
+        gdfShortestPath[col] = gdfShortestPath[col].apply(
+            lambda x: x.tolist() if isinstance(x, np.ndarray) else x
+        )
+        
+    return jsonify(gdfShortestPath.to_json()), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
